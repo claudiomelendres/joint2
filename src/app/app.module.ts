ac2d4400c3a9863b390da03366b3069c96fe8f85
Claudio Melendres <claudiomelendres@gmail.com>
2018-10-02 00:14:43 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Pagina1Component } from './pagina1/pagina1.component';
+import { UmlComponent } from './uml/uml.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina1Component,
+    UmlComponent
   ],
   imports: [
     BrowserModule
