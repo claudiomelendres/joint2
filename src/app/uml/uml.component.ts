3c6d54e1d33f265fc3f653f2151ab4efbfd55153
Claudio Melendres <claudiomelendres@gmail.com>
2018-10-03 00:36:37 -0400

    drag

--- src/app/uml/uml.component.ts
+++ src/app/uml/uml.component.ts
@@ -14,15 +14,114 @@ export class UmlComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const graph = new joint.dia.Graph();
 
-    const paper = new joint.dia.Paper({
-    el: document.getElementById('paperUml'),
-    width: 1800,
-    height: 1600,
-    gridSize: 1,
+    const graph = new joint.dia.Graph(),
+      paper = new joint.dia.Paper({
+    el: jQuery('#paperY'),
     model: graph
+  });
+    const stencilGraph = new joint.dia.Graph,
+    stencilPaper = new joint.dia.Paper({
+    el: jQuery('#stencil'),
+    height: 60,
+    model: stencilGraph,
+    interactive: false
+  });
+
+  const r1 = new joint.shapes.basic.Rect({
+    position: {
+      x: 10,
+      y: 10
+    },
+    size: {
+      width: 100,
+      height: 40
+    },
+    attrs: {
+      text: {
+        text: 'Rect1'
+      }
+    }
+  });
+  const r2 = new joint.shapes.basic.Rect({
+    position: {
+      x: 120,
+      y: 10
+    },
+    size: {
+      width: 100,
+      height: 40
+    },
+    attrs: {
+      text: {
+        text: 'Rect2'
+      }
+    }
+  });
+  stencilGraph.addCells([r1, r2]);
+// ---------------------------
+stencilPaper.on('cell:pointerdown', function(cellView, e, x, y) {
+    jQuery('body').append('<div id="flyPaper" style="position:fixed;z-index:100;opacity:.7;pointer-event:none;"></div>');
+    const flyGraph = new joint.dia.Graph,
+      flyPaper = new joint.dia.Paper({
+        el: jQuery('#flyPaper'),
+        model: flyGraph,
+        interactive: false
+      }),
+      flyShape = cellView.model.clone(),
+      pos = cellView.model.position(),
+      offset = {
+        x: x - pos.x,
+        y: y - pos.y
+      };
+
+    flyShape.position(0, 0);
+    flyGraph.addCell(flyShape);
+    jQuery('#flyPaper').offset({
+      left: e.pageX - offset.x,
+      top: e.pageY - offset.y
     });
+    jQuery('body').on('mousemove.fly', function(element) {
+        jQuery('#flyPaper').offset({
+        left: element.pageX - offset.x,
+        top: element.pageY - offset.y
+      });
+    });
+    jQuery('body').on('mouseup.fly', function(element) {
+      // tslint:disable-next-line:no-shadowed-variable
+      const x = element.pageX,
+        // tslint:disable-next-line:no-shadowed-variable
+        y = element.pageY,
+        target = paper.$el.offset();
+// Dropped over paper ?
+if (
+    x > target.left &&
+    x < target.left + paper.$el.width() &&
+    y > target.top &&
+    y < target.top + paper.$el.height()
+  ) {
+    const s = flyShape.clone();
+    s.position(x - target.left - offset.x, y - target.top - offset.y);
+    graph.addCell(s);
+  }
+  jQuery('body')
+    .off('mousemove.fly')
+    .off('mouseup.fly');
+  flyShape.remove();
+  jQuery('#flyPaper').remove();
+});
+});
+
+    // -----------------UML
+//     const graph = new joint.dia.Graph();
+
+//     const paper = new joint.dia.Paper({
+//     el: document.getElementById('paperUml'),
+//     width: 1800,
+//     height: 1600,
+//     gridSize: 1,
+//     model: graph
+//     });
 
     const uml = joint.shapes.uml;
 
@@ -33,7 +132,7 @@ export class UmlComponent implements OnInit {
         size: { width: 240, height: 100 },
         name: ['Mammal'],
         attributes: ['dob: Date'],
-        methods: ['+ setDateOfBirth(dob: Date): Void','+ getAgeAsDays(): Numeric'],
+        methods: ['+ setDateOfBirth(dob: Date): Void', '+ getAgeAsDays(): Numeric'],
         attrs: {
             '.uml-class-name-rect': {
                 fill: '#feb662',
@@ -60,7 +159,7 @@ export class UmlComponent implements OnInit {
     }),
 
     person: new uml.Abstract({
-        position: { x:300  , y: 300 },
+        position: { x: 300  , y: 300 },
         size: { width: 260, height: 100 },
         name: ['Person'],
         attributes: ['firstName: String', 'lastName: String'],
